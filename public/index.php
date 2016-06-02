b9b6506d09dfebf069ff12bd3bd79fdd5f2a4eff
Wesley Willians <wesleywillians@gmail.com>
2016-06-02 09:31:26 -0400

    Hello world 3

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function() use($app) {
-    return "Hello World 2";
+    return "Hello World 3";
 });
 
 $app->run();
